af3dc49c1cb49e7e46c650e88df39174add571f6
ehdry <ehdry93125@nate.com>
2015-11-01 17:21:28 +0900

    imageview 다시

--- image/src/main/java/com/example/com/myapplication/MainActivity.java
+++ image/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.com.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -10,17 +13,29 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
-    TextView choicetext;
-    Switch switchbtn;
-    RadioGroup Group;
-    RadioButton jelly;
-    RadioButton kit;
-    RadioButton lol;
-    Button exitbtn;
-    Button resetbtn;
-    ImageView I;
+public class MainActivity extends Activity {
+    private TextView choicetext;
+    private Switch switchbtn;
+    private RadioGroup Group;
+    private RadioButton jelly, kit, lol;
+    private Button exitbtn , resetbtn;
+    private ImageView I;
 
+    public void showVisible(Boolean ischecked){
+        if(ischecked){
+            Group.setVisibility(android.view.View.VISIBLE);
+            I.setVisibility(android.view.View.VISIBLE);
+            exitbtn.setVisibility(android.view.View.VISIBLE);
+            resetbtn.setVisibility(android.view.View.VISIBLE);
+        }
+        else{
+            Group.setVisibility(android.view.View.INVISIBLE);
+            I.setVisibility(android.view.View.INVISIBLE);
+            exitbtn.setVisibility(android.view.View.INVISIBLE);
+            resetbtn.setVisibility(android.view.View.INVISIBLE);
+            switchbtn.setChecked(false);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +50,42 @@ public class MainActivity extends AppCompatActivity {
         exitbtn = (Button)findViewById(R.id.exit);
        resetbtn = (Button)findViewById(R.id.reset);
         I = (ImageView)findViewById(R.id.image1);
+
+        switchbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showVisible(isChecked);
+            }
+        });
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        resetbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showVisible(false);
+            }
+        });
+        jelly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                I.setImageResource(R.mipmap.jellybean);
+            }
+        });
+        kit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                I.setImageResource(R.mipmap.kitkat);
+            }
+        });
+        lol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                I.setImageResource(R.mipmap.lollipop);
+            }
+        });
     }
 }
