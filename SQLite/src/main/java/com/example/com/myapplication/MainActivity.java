f2851f562ca53ef71fc4d0e8c3a643333322e38e
ehdry <ehdry93125@nate.com>
2015-12-10 18:43:32 +0900

    sql

---
+++ SQLite/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.com.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    myDBHelper myHelper;
+    EditText edtName,edtNumber,edtNameResult,edtNumberResult;
+    Button btnInit,btnInsert,btnSelect,btnChange,btnDelete;
+    SQLiteDatabase sqlDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtName = (EditText)findViewById(R.id.edtName);
+        edtNumber = (EditText)findViewById(R.id.edtNumber);
+        edtNameResult = (EditText)findViewById(R.id.edtNameResult);
+        edtNumberResult = (EditText)findViewById(R.id.edtNumberResult);
+        btnInit = (Button)findViewById(R.id.btnInit);
+        btnInsert = (Button)findViewById(R.id.btnInsert);
+        btnSelect = (Button)findViewById(R.id.btnSelect);
+        btnChange = (Button)findViewById(R.id.btnChange);
+        btnDelete = (Button)findViewById(R.id.btnDelete);
+
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB,1,2);
+                sqlDB.close();
+            }
+        });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();// 스트링은 ' ' 로 묶어야지
+                sqlDB.execSQL("INSERT INTO groupTBL VALUES ( '"
+                + edtName.getText().toString()+ "',"
+                + edtNumber.getText().toString() + ");");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(),"입력됨",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;",null);
+
+                String strNames = "그룹 이름" +"\r\n";
+                String strNumbers = "인원"+"\r\n";
+
+                while(cursor.moveToNext()){
+                    strNames += cursor.getString(0) +"\r\n";
+                    strNumbers += cursor.getString(1) +"\r\n";
+                }
+                edtNameResult.setText(strNames);
+                edtNumberResult.setText(strNumbers);
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+    }
+    public class myDBHelper extends SQLiteOpenHelper{
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE group ( gName CHAR(20) PRIMARY KEY, gNumber INTEGER);");
+        }
+
+        public myDBHelper(Context context){
+            super(context,"groupDB",null,1);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
